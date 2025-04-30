3f1a5a0e75a0ee5cc27f79b1ae92557f5bc2331f
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-04-29 20:19:15 -0400

    tests: make 100% cover in all project and finalizing tests

--- src/presentation/controllers/orders/orders.controller.test.ts
+++ src/presentation/controllers/orders/orders.controller.test.ts
@@ -2,6 +2,7 @@ import type { Context } from '@/presentation/protocols/http'
 import { OrdersRouter } from '.'
 import { FileClient } from '@/infra/file'
 import { OrderClient } from '@/infra/order'
+import { ZodError } from 'zod'
 
 const makeSut = () => {
   const sut = new OrdersRouter()
@@ -9,6 +10,41 @@ const makeSut = () => {
     sut,
   }
 }
+
+const makeFileClientStub = (): FileClient => {
+  class FileClientStub extends FileClient {
+    verify = vi.fn().mockReturnValue([])
+    read = vi.fn().mockReturnValue([])
+  }
+
+  return new FileClientStub()
+}
+
+const makeOrderClientStub = (): OrderClient => {
+  class OrderClientStub extends OrderClient {
+    verifyFilters = vi.fn().mockReturnValue({})
+    processRaw = vi.fn().mockReturnValue([])
+    group = vi.fn().mockReturnValue([])
+    getOrderBetweenDates = vi.fn().mockReturnValue([])
+  }
+
+  return new OrderClientStub()
+}
+
+const files = [
+  {
+    fieldname: 'file',
+    originalname: 'invalid-file.txt',
+    encoding: 'utf-8',
+    mimetype: 'text/plain',
+    buffer: Buffer.from(''),
+    size: 0,
+    destination: '',
+    filename: 'invalid-file.txt',
+    path: '',
+    stream: {} as unknown as Express.Multer.File['stream'],
+  },
+]
 
 const makeCtx = (): Context => {
   return {
@@ -21,8 +57,8 @@ const makeCtx = (): Context => {
     url: '/orders',
     body: {},
     clients: {
-      file: vi.mocked(new FileClient()), // Mock FileClient
-      order: vi.mocked(new OrderClient()), // Mock OrderClient
+      file: makeFileClientStub(),
+      order: makeOrderClientStub(),
     },
   }
 }
@@ -53,6 +89,8 @@ describe('OrdersController', () => {
       orderId: 'invalid-order-id',
     }
 
+    ctx.clients.order.verifyFilters = vi.fn().mockReturnValue(new ZodError([]))
+
     const response = await sut.handle(ctx)
 
     expect(response.statusCode).toBe(400)
@@ -62,23 +100,190 @@ describe('OrdersController', () => {
     const { sut } = makeSut()
     const ctx = makeCtx()
 
-    ctx.req.files = [
-      {
-        fieldname: 'file',
-        originalname: 'invalid-file.txt',
-        encoding: 'utf-8',
-        mimetype: 'text/plain',
-        buffer: Buffer.from(''),
-        size: 0,
-        destination: '',
-        filename: 'invalid-file.txt',
-        path: '',
-        stream: {} as unknown as Express.Multer.File['stream'],
-      },
-    ]
+    ctx.req.files = files
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(new ZodError([]))
 
     const response = await sut.handle(ctx)
 
     expect(response.statusCode).toBe(400)
+  })
+
+  test('should return 400 if processRaw is invalid', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.files = files
+
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue(new ZodError([]))
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('should return 400 if group is invalid', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.files = files
+
+    ctx.clients.order.group = vi.fn().mockReturnValue(new ZodError([]))
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('should return 200 if filters startDate and endDate are valid', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.query = {
+      startDate: '2023-01-01',
+      endDate: '2023-12-31',
+    }
+
+    ctx.clients.order.verifyFilters = vi.fn().mockReturnValue({
+      startDate: new Date('2023-01-01'),
+      endDate: new Date('2023-12-31'),
+    })
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue([])
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('should return 200 if filters orderId is valid', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.query = {
+      orderId: 'valid-order-id',
+    }
+
+    ctx.clients.order.verifyFilters = vi.fn().mockReturnValue({
+      orderId: 'valid-order-id',
+    })
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue([])
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('should return 200 if filters are empty', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.query = {}
+
+    ctx.clients.order.verifyFilters = vi.fn().mockReturnValue({})
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue([])
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('should return 500 if an unexpected error occurs', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.files = files
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockImplementation(() => {
+      throw new Error('Unexpected error')
+    })
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(500)
+  })
+
+  test('should return empty data if no orders are found for the given orderId', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.query = {
+      orderId: 'non-existing-order-id',
+    }
+
+    ctx.clients.order.verifyFilters = vi.fn().mockReturnValue({
+      orderId: 'non-existing-order-id',
+    })
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue([])
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    ctx.clients.order.getOrderById = vi.fn().mockReturnValue(undefined)
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      data: [],
+    })
+  })
+
+  test('should return a valid responde for a existing orderId', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.query = {
+      orderId: 'non-existing-order-id',
+    }
+
+    ctx.clients.order.verifyFilters = vi.fn().mockReturnValue({
+      orderId: 'non-existing-order-id',
+    })
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue([])
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    ctx.clients.order.getOrderById = vi.fn().mockReturnValue({})
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      data: [{}],
+    })
+  })
+
+  test('should return 200 if everything is ok', async () => {
+    const { sut } = makeSut()
+    const ctx = makeCtx()
+
+    ctx.req.files = files
+
+    ctx.clients.file.verify = vi.fn().mockReturnValue(files)
+    ctx.clients.file.read = vi.fn().mockReturnValue([])
+    ctx.clients.order.processRaw = vi.fn().mockReturnValue([])
+    ctx.clients.order.group = vi.fn().mockReturnValue([])
+
+    const response = await sut.handle(ctx)
+
+    expect(response.statusCode).toBe(200)
   })
 })
